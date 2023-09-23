4307ef3a1f7f1d0dd7b0eee5cf1acbe5c80009ba
Maicon Santana <msn-maicon@hotmail.com>
2023-09-23 05:30:20 -0300

    Adc + lista

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form-agenda');
-//const contato = [];
+let linhas = '';
 
 form.addEventListener('submit' , function(e){
     e.preventDefault();
@@ -7,61 +7,18 @@ form.addEventListener('submit' , function(e){
     const inputContato = document.getElementById ('contato');
     const inputTelefone = document.getElementById ('telefone');
 
-   // adicionaLinha();
-    //atualizaTabela();
+
     let linha = '<tr>';
     linha += `<td>${inputContato.value} </td>`;
     linha += `<td>${inputTelefone.value} </td>`;
     linha += '<tr>';
 
-    alert(` O Contato: ${inputContato.value} foi inserido na sua agenda!`);
-})
-/*
-function adicionaLinha () {
-
-
-
-    if(atividades.includes(inputNomeAtividade.value)){
-        
-    } else {
-
-        atividades.push(inputNomeAtividade.value);
-    notas.push(parseFloat(inputNotaAtividade.value));
-
-    let linha = '<tr>';
-    linha += `<td>${inputNomeAtividade.value} </td>`;
-    linha += `<td>${inputNotaAtividade.value} </td>`;
-
-
     linhas += linha;
 
-    }
-
-    inputNomeAtividade.value = '';
-    inputNotaAtividade.value = '';
-
-}
-
-function atualizaTabela (){
-    const corpoTabela = document.querySelector ('tbody');
+    const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
-}
 
-function atualizaMediaFinal(){
-    const mediaFinal = calculaMediaFinal();
+    inputContato.value = '';
+    inputTelefone.value = '';
     
-    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2);
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
-}
-
-function calculaMediaFinal(){
-    
-        let somaDasNotas = 0;
-
-    for (let i = 0; i < notas.length; i++) {
-        somaDasNotas += notas[i];
-    } 
-
-     return somaDasNotas / notas.length;
-    
-}*/
+})
